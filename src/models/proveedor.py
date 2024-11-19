ef3799883219cbd9ad6fdce27461b3c317dc02cb
Leymar <buenaventuraleymar901@gmail.com>
2024-11-19 16:19:08 -0500

    se creo la estructura interna de la calase proveedor, logica

---
+++ src/models/proveedor.py
@@ -0,0 +1,13 @@
+class Proveedor:
+
+    def __init__(self, nombre, direccion, telefono, lista):
+        self.nombre_proveedor = nombre
+        self.direccion = direccion
+        self.telefono = telefono
+        self.lista_productos_suministrados = lista
+
+    def agregar_produto_suministros(self):
+        pass
+
+    def eliminar_produto_suministros(self):
+        pass
