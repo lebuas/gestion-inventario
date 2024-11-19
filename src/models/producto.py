030516f6e9ba69fc911f13a8df906af396f216d5
Leymar <buenaventuraleymar901@gmail.com>
2024-11-19 16:08:38 -0500

    se creo el resto de modulos, bodega, categoria, producto

---
+++ src/models/producto.py
@@ -0,0 +1,12 @@
+class Producto:
+    def __init__(self, nombre, descripcion, precio, stock_inicial):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self. precio = precio
+        self.stock_inicial = stock_inicial
+
+    def aumenta_stock(self):
+        pass
+
+    def disminuir_stock(self):
+        pass
