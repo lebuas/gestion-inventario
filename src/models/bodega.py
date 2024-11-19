4ce8fd0ea926952ec4acfebb694ab9520cd9b774
Leymar <buenaventuraleymar901@gmail.com>
2024-11-19 16:13:22 -0500

    se creo la estructura interna de la calase bodega, logica

---
+++ src/models/bodega.py
@@ -0,0 +1,13 @@
+class Bodega:
+
+    def __init__(self, nombre, ubicacion, capacidad, lista):
+        self.nombre = nombre
+        self.ubicacion = ubicacion
+        self.capacidad_maxima = capacidad
+        self.lista_productos_almacenado = lista
+
+    def agregar_producto_a_bodega(self):
+        pass
+
+    def retir_producto_bodega(self):
+        pass
