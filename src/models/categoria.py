059ee601d86b30383dafdda04f19f68d2f7e637c
Leymar <buenaventuraleymar901@gmail.com>
2024-11-19 16:16:13 -0500

    se creo la estructura interna de la calase categoria, logica

---
+++ src/models/categoria.py
@@ -0,0 +1,12 @@
+class Categoria():
+
+    def __init__(self, nombre, descripcion, lista):
+        self.nombre_categoira = nombre
+        self.descripcion_categoria = descripcion
+        self.lista_productos_alamacenados = lista
+
+    def agregar_producto_categoria_existente():
+        pass
+
+    def elimiar_producto_categoria_existente():
+        pass
